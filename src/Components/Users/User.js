cc6f55bc6680675d47c454e54593e57cfae5518f
Fabio Gomez <fabiogomez11c@gmail.com>
2021-08-26 17:26:55 -0500

    User and users created

---
+++ src/Components/Users/User.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const User = () => {
+    return (
+        <div className="user__info">
+            <img/>
+            <div className="user__name">
+                <p>
+                    Name
+                </p>
+            </div>
+            <div className="user__profile">
+                <a>
+                    Profile
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { User } from './User'
 
 export const Users = () => {
     return (
-        <div>
-            
+        <div className="user-section">
+            <div className="users">
+                <User/>
+            </div>
         </div>
     )
 }
